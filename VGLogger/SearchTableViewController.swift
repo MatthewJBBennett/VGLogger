44528b0ec3cb08a41385dbaa03c8915de2863f54
Matthew Bennett <matthew@Matthews-MacBook-Pro.local>
2017-11-05 19:17:31 -0800

    test

--- VGLogger/SearchTableViewController.swift
+++ VGLogger/SearchTableViewController.swift
@@ -97,7 +97,7 @@ class SearchTableViewController: UITableViewController, UISearchBarDelegate {
             //debugPrint(request)
         }
     }
-    
+      
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
