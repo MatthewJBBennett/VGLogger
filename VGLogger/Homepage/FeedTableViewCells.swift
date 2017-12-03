ed0c4edec13c88f758fb0eca6cdd46fb6652cc1c
student <student@ssu-cs-dar28-167-219.sonoma.edu>
2017-12-03 14:36:51 -0800

    temp

---
+++ VGLogger/Homepage/FeedTableViewCells.swift
@@ -0,0 +1,72 @@
+//
+//  FeedTableViewCells.swift
+//  VGLogger
+//
+//  Created by student on 12/3/17.
+//  Copyright © 2017 SSU470. All rights reserved.
+//
+
+import UIKit
+
+class PopularGameTableViewCell: UITableViewCell
+{
+    
+ //   @IBOutlet weak var collectionview: UICollectionView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+}
+
+class NewsTableViewCell: UITableViewCell
+{
+    
+//    @IBOutlet weak var articleTitleLabel: UILabel!
+//    @IBOutlet weak var authorLabel: UILabel!
+//    @IBOutlet weak var articleImage: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+}
+
+//extension PopularGameTableViewCell : UICollectionViewDataSource
+//{
+//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell
+//    {
+//        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "horizontalGameCell", for: indexPath as IndexPath) as! UICollectionViewCell
+//        return cell
+//    }
+//
+//
+//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+//        return 4
+//    }
+//}
+//
+//extension PopularGameTableViewCell : UICollectionViewDelegateFlowLayout
+//{
+//
+//    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+//        let itemsPerRow:CGFloat = 4
+//        let hardCodedPadding:CGFloat = 5
+//        let itemWidth = (collectionView.bounds.width / itemsPerRow) - hardCodedPadding
+//        let itemHeight = collectionView.bounds.height - (2 * hardCodedPadding)
+//        return CGSize(width: itemWidth, height: itemHeight)
+//    }
+//}
+
---
+++ VGLogger/Models/popularGame.swift
@@ -0,0 +1,87 @@
+//
+//  popularGame.swift
+//  VGLogger
+//
+//  Created by student on 12/3/17.
+//  Copyright © 2017 SSU470. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+import SwiftyJSON
+
+class popularGame: NSObject
+{
+    let apiKey = "2c4b6af8bd50607b85c8bc1813cb8fa5"
+    var vgDS: VideoGameDataSource?
+    // var json: JSON = JSON.null
+    var rating: Float?
+    //var name: String?
+    
+    @objc dynamic var dataFromServer: [AnyObject]?
+    
+    //URL for popular game data
+    //https://api-2445582011268.apicast.io/games/?fields=name,popularity,total_rating,cover&order=popularity:desc
+    
+    //URL for pulse news
+    //https://api-2445582011268.apicast.io/pulses/?fields=title,published_at,author,image,url&order=published_at:desc
+    
+    //news
+    func downloadData() {
+        if let url = URL(string: "https://api-2445582011268.apicast.io/pulses/?fields=title,published_at,author,image,url&order=published_at:desc") {
+            var urlRequest = URLRequest(url: url)
+            urlRequest.httpMethod = HTTPMethod.get.rawValue
+            urlRequest.addValue(apiKey, forHTTPHeaderField: "user-key")
+            urlRequest.addValue("application/json", forHTTPHeaderField: "Accept")
+            
+            Alamofire.request(urlRequest).responseJSON {response in print(response)
+                if let allSections = response.result.value as? [[String : Any]]
+                {
+                    //Here we will loop through the array and parse each dictionary
+                    for section in allSections
+                    {
+                        if let author = section["author"] as? String, let image = section["image"] as? String, let title = section["title"] as? String, let url = section["url"] as? String
+                        {
+                            print(url)
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    //popular games
+    //    func downloadData() {
+    //        if let url = URL(string: "https://api-2445582011268.apicast.io/games/?fields=name,popularity,total_rating,cover&order=popularity:desc") {
+    //            var urlRequest = URLRequest(url: url)
+    //            urlRequest.httpMethod = HTTPMethod.get.rawValue
+    //            urlRequest.addValue(apiKey, forHTTPHeaderField: "user-key")
+    //            urlRequest.addValue("application/json", forHTTPHeaderField: "Accept")
+    //
+    //            Alamofire.request(urlRequest).responseJSON {response in print(response)
+    //                if let allSections = response.result.value as? [[String : Any]]
+    //                {
+    //                    //Here we will loop through the array and parse each dictionary
+    //                    for section in allSections
+    //                    {
+    //                        if let name = section["name"] as? String, let cover = section["cover"] as? [String : Any], let imageUrl = cover["url"] as? String
+    //                            //let rating = section["total_rating"] as? Float
+    //                        //guard let rating = section["total_rating"] as? String else{return}
+    //                        {
+    //                            //let rating = section["total_rating"] as? Float
+    //                            //let outrating = Int(rating.rounded())   //Round the float rating up or down, then convert to int for output
+    ////                            for i in 0..<imageUrl.count
+    ////                            {
+    ////                                if imageUrl[i].contains("https")
+    ////                                {
+    ////
+    ////                                }
+    ////                            }
+    //                        }
+    //                    }
+    //                }
+    //            }
+    //        }
+    //    }
+}
+
